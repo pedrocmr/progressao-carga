0a4e20661169b9e814920a38f63c002861f3e49b
pedrocmr <pedromooreira@gmail.com>
2024-06-05 18:46:44 -0300

    Project init

---
+++ src/main/java/com/progressao_carga/ProgressaoDeCargaApplication.java
@@ -0,0 +1,13 @@
+package com.progressao_carga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressaoDeCargaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressaoDeCargaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progressao_carga/ProgressaoDeCargaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progressao_carga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressaoDeCargaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
